40071425fbbbfce081aeb4f03f7e68de2bf1c575
Yury Marin <yurimar1532@gmail.com>
2023-03-25 22:36:46 -0500

    V1.0.0 Documentos base

---
+++ calificaciones.js
@@ -0,0 +1,8 @@
+
+function calcular_definitiva(p1,p2,p3,e1,t1){
+    // TO DO: No se verifico la validez del algoritmo
+    let promedio=0.55*(p1+p2+p3) + 0.3*e1+0.15*t1    
+    return promedio;
+}
+
+
